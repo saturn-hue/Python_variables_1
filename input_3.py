9116e809beeeee5cf7ee7c700b56dc4c4a2bbbdf
Saturday Okposio <ausjohn003@yahoo.com>
2026-06-05 16:04:51 +0100

    Adding python input

---
+++ input_3.py
@@ -0,0 +1,8 @@
+""" Input in Python """
+user_first_name = input("What is your name?:  ")
+
+user_last_name = input("What is your last name? : ")
+
+# Concatenating the variables to print the full name
+print("Hello " + user_first_name + " " + user_last_name)
+
